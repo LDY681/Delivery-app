e0988e1e578fb5291df2b33a591ea2ece181427d
Anthony Ko <anthonyko@pal-nat186-54-238.itap.purdue.edu>
2018-12-03 16:52:51 -0500

    added firebase code

---
+++ firebase/login.js
@@ -0,0 +1,76 @@
+
+function loginUser(userType) {
+  var mail;
+  var pass;
+  if(userType == "deliv"){
+    mail = "demail";
+    pass = "dpass";
+  } else {
+    mail = "remail";
+    pass = "rpass";
+  }
+  var userEmail = document.getElementById(mail).value;
+  var userPass = document.getElementById(pass).value;
+  
+  firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if(errorMessage != "A network error (such as timeout, interrupted connection or unreachable host) has occurred."){
+      window.alert("Error: " + errorMessage);
+    }
+    signOutUser();
+  });
+}
+
+function changePage(userType) {
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      console.log(user.email);
+      if(userType == "deliv"){
+        window.location.href = ("driver.html");
+      } else {
+        window.location.href = ("restaurant.html");
+      }
+      
+    } else {
+      
+    }
+   });
+ }
+
+function createUser(userType) {
+  var mail;
+  var pass;
+  if(userType == "deliv"){
+    mail = "demail";
+    pass = "dpass";
+  } else {
+    mail = "remail";
+    pass = "rpass";
+  }
+  var userEmail = document.getElementById(mail).value;
+  var userPass = document.getElementById(pass).value;
+
+  firebase.auth().createUserWithEmailAndPassword(userEmail, userPass).catch(function(error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+
+    window.alert("Error: " + errorMessage);
+  });
+  window.alert("Account created for " + userEmail);
+  changePage(userType);
+}
+
+
+function signOutUser() {
+
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+    window.location.href="index.html";
+  }).catch(function(error) {
+    // An error happened.
+    window.alert("Error: " + error.message);
+  });
+
+}
